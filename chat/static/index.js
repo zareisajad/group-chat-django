a3f00e99a9f2c7e2c32a6a849e655443dea0cc76
sajad <zareisajad@protonmail.com>
2022-02-06 18:47:54 +0330

    update

--- chat/static/index.js
+++ chat/static/index.js
@@ -1,11 +1,6 @@
 // Automatically set the focus to the room name input
 document.querySelector('#room-name-input').focus();
-// If you hit "enter" on the keyboard, trigger the click method
-document.querySelector('#room-name-input').onkeyup = (e) => {
-    if (e.keyCode === 13) {
-        document.querySelector('#room-name-submit').click();
-    }
-};
+
 // When you submit the form, redirect the user to the room page
 document.querySelector('#room-name-submit').onclick = (e) => {
     var roomName = document.querySelector('#room-name-input').value;
