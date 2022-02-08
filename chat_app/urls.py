d60dc8d1cd058301a1ab5c01bb3907ceb053e0ca
Sajad Zarei <zareisajad@protonmail.com>
2022-02-08 16:59:45 +0330

    Update urls.py

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls', namespace='chat')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
