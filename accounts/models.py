d768587ac1e315d2097981f97dd2ac308ec5eea6
Sajad Zarei <zareisajad@protonmail.com>
2022-02-08 16:58:17 +0330

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -27,14 +27,3 @@ class User(AbstractBaseUser):
     @property
     def is_staff(self):
         return self.is_admin
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    photo = models.ImageField(default='profiles/default.png', upload_to='profiles')
-    about = models.TextField()
-    fname = models.CharField(max_length=300)
-    lname = models.CharField(max_length=300)
-
-    def __str__(self):
-        return f'{self.user.username} Profile'
